d4829fdff88ffbf7dc84d3e76fa44d9d7d715acd
Luis David Garcia Lunar <ldgl1215@gmail.com>
2020-12-07 23:36:35 -0400

    init commit

---
+++ app/src/main/java/com/example/carlosapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.carlosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.carlosapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_fragment)
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
